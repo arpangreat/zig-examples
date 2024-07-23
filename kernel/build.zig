8e7a83d264eada0b6524548ab0828b4398ea81ff
arpangreat <arpanthegreat41@gmail.com>
2024-07-23 17:16:21 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- kernel/build.zig
+++ kernel/build.zig
@@ -10,14 +10,13 @@ pub fn build(b: *Builder) void {
     var disabled_features = Feature.Set.empty;
     var enabled_features = Feature.Set.empty;
 
-    disabled_features.addFeature(@enumToInt(features.mmx));
-    disabled_features.addFeature(@enumToInt(features.sse));
-    disabled_features.addFeature(@enumToInt(features.sse2));
-    disabled_features.addFeature(@enumToInt(features.avx));
-    disabled_features.addFeature(@enumToInt(features.avx2));
-    enabled_features.addFeature(@enumToInt(features.soft_float));
+    disabled_features.addFeature(@intFromEnum(features.sse));
+    disabled_features.addFeature(@intFromEnum(features.sse2));
+    disabled_features.addFeature(@intFromEnum(features.avx));
+    disabled_features.addFeature(@intFromEnum(features.avx2));
+    enabled_features.addFeature(@intFromEnum(features.soft_float));
 
-    const target = CrossTarget{ .cpu_arch = Target.Cpu.aarch.i386, .os_tag = Target.Os.Tag.freestanding, .abi = Target.Abi.none, .cpu_features_sub = disabled_features, .cpu_features_add = enabled_features };
+    const target = CrossTarget{ .cpu_arch = Target.Cpu.Arch.i386, .os_tag = Target.Os.Tag.freestanding, .abi = Target.Abi.none, .cpu_features_sub = disabled_features, .cpu_features_add = enabled_features };
 
     const mode = b.standardReleaseOptions();
 
